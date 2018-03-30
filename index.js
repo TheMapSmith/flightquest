fd4b331d7f50f6de58fb4f1bf5d66b28ebbe930d
Stephen Smith <stephen@mapsmith.net>
2018-03-30 07:09:58 -0400

    electron reload

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('electron-reload')(__dirname);
+
 const {app, BrowserWindow} = require('electron')
   const path = require('path')
   const url = require('url')
@@ -8,7 +10,7 @@ const {app, BrowserWindow} = require('electron')
 
   function createWindow () {
     // Create the browser window.
-    win = new BrowserWindow({width: 800, height: 600})
+    win = new BrowserWindow({width: 900, height: 700})
 
     // and load the index.html of the app.
     win.loadURL(url.format({
