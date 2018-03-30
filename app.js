d28c478fd3be67211509f45acd7dcc119671c999
Stephen Smith <stephen@mapsmith.net>
2018-03-30 11:26:02 -0400

    get coords of all requested airports

--- app.js
+++ app.js
@@ -5,13 +5,25 @@ var airports = ['BTV', 'ATL']
 const airportCodes = JSON.parse(fs.readFileSync('data/airportCodes.json', 'utf-8'))
 const airportData = JSON.parse(fs.readFileSync('data/airportData.json', 'utf-8'))
 
-getIATA();
+getAirportCoords();
 
-function getIATA() {
-  for (var i = 0; i < airports.length; i++) {
-    var codeIndex = airportCodes.indexOf(airports[i])
-    if (codeIndex > 0) {
-      console.log(airportData[codeIndex]);
+function getAirportCoords() {
+  var allCoords = []
+  for (var i = 0; i <= airports.length; i++) {
+    if (i != airports.length) {
+      var codeIndex = airportCodes.indexOf(airports[i])
+      if (codeIndex > 0) {
+        var lnglat = []
+        lnglat.push(airportData[codeIndex].Longitude)
+        lnglat.push(airportData[codeIndex].Latitude)
+        allCoords.push(lnglat)
+      }
+    } else {
+      setbbox(allCoords)
     }
   }
+}
+
+function setbbox(allCoords) {
+  console.log(allCoords);
 }
