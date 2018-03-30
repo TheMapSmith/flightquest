5917ed9fc6ec13f84a315b16a11a9916efc1cf50
Stephen Smith <stephen@mapsmith.net>
2018-03-30 11:50:38 -0400

    start logic for bbox

--- app.js
+++ app.js
@@ -25,7 +25,29 @@ function getAirportCoords() {
 }
 
 function setbbox(allCoords) {
-  for (var i = 0; i < allCoords.length; i++) {
-    var oneCoordPair = allCoords[i]
+  var minlng, maxlng, minlat, maxlat
+  var noPairs = allCoords.length
+  if (noPairs == 2) {
+    var firstPair = allCoords[0]
+    var secondPair = allCoords[1]
+
+    var lats = []
+    var lngs = []
+
+    lngs.push(firstPair[0])
+    lngs.push(secondPair[0])
+    // TODO: MAX of lngs
+    lats.push(firstPair[1])
+    lats.push(secondPair[1])
+    // TODO: MAX of lats
+
+  } else if (noPairs > 2) {
+    // TODO: Probably different logic
+  } else if (noPairs == 0) {
+    console.log("No airports");
+  } else if (noPairs == 1) {
+    // TODO: Set the center of the map here
   }
+
 }
+// TODO: Pass the BBOX to the Map object in HTML
