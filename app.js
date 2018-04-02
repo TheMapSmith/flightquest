9f34437ae54da7440a3946ce577ae6ebe074d103
Stephen Smith <stephen@mapsmith.net>
2018-04-01 22:02:24 -0500

    display the path on the map

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ var airports = ['BTV', 'ATL']
 const airportCodes = JSON.parse(fs.readFileSync('data/airportCodes.json', 'utf-8'))
 const airportData = JSON.parse(fs.readFileSync('data/airportData.json', 'utf-8'))
 
-// getAirportCoords();
-
 function getAirportCoords() {
   var allCoords = []
   for (var i = 0; i <= airports.length; i++) {
@@ -24,4 +22,22 @@ function getAirportCoords() {
       return allCoords
     }
   }
+}
+
+function makePathFeature (line) {
+  var lineFeature = {}
+  lineFeature.id = airports[0] + ' to ' + airports[1]
+  lineFeature.type = 'line'
+  lineFeature.source = {}
+  lineFeature.source.type = "geojson"
+  lineFeature.properties = {}
+  lineFeature.source.data = line
+  lineFeature.layout = {}
+  lineFeature.layout['line-join'] = 'round'
+  lineFeature.layout['line-cap'] = 'round'
+  lineFeature.paint = {}
+  lineFeature.paint['line-color'] = '#ffdd00'
+  lineFeature.paint['line-width'] = 9
+
+  return lineFeature
 }
