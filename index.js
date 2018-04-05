670cb8c83867f9086438068e54cacde92061a440
Stephen Smith <stephen@mapsmith.net>
2018-04-04 23:24:28 -0400

    frameless

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const {app, BrowserWindow} = require('electron')
 
   function createWindow () {
     // Create the browser window.
-    win = new BrowserWindow({width: 900, height: 700})
+    win = new BrowserWindow({width: 900, height: 700, frame: false})
 
     // and load the index.html of the app.
     win.loadURL(url.format({
