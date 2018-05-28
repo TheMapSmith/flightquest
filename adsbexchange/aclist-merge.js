fe6b75de5b9614d4a0e1ed7ebf5de56290418b7a
Stephen Smith <stephen@mapsmith.net>
2018-05-27 23:37:13 -0400

    Refactor flight parser
    Hugely restructured for readability and efficency

--- adsbexchange/aclist-merge.js
+++ adsbexchange/aclist-merge.js
@@ -1,84 +1,109 @@
-const fs = require('fs')
-const turf = require('@turf/turf')
-const path = require('path')
+const fs = require('fs');
+const path = require('path');
 
-// const scrapeFolder = 'adsbexchange/scrape/'
-const scrapeFolder = 'scrape/'
+const dumpFolder = 'dump/2018-05-16/vt/';
 
-var allCoords = []
-var geojson = {
-  "type": "FeatureCollection",
-  "features": []
+const vtFlights = {};
+
+const vtbbox = {
+	long: {
+		min: -73.5,
+		max: -71.5
+	},
+	lat: {
+		min: 42.7,
+		max: 45
+	}
+};
+
+const geojson = {
+	type: 'FeatureCollection',
+	features: []
+};
+
+listFiles(dumpFolder);
+
+function listFiles(folder) {
+	fs.readdir(folder, (err, files) => {
+		if (err) {
+			console.log(err);
+		} else {
+			const scrapeFiles = files.filter(file => {
+				return path.extname(file).toLowerCase() === '.json';
+			}).map(file => {
+				return dumpFolder + file;
+			});
+			fileIterate(scrapeFiles);
+		}
+	});
 }
 
-var scrapeFolderFiles = fs.readdir(scrapeFolder, (err, files) => {
-  if (err) {
-    console.log(err);
-  } else {
-    procFiles(files)
-  }
-})
-
-function procFiles(files){
-    var scrapeFiles = files.filter((file) => {
-      return path.extname(file).toLowerCase() === '.json';
-    }).map((file) => {
-      return scrapeFolder + file
-    })
-  fileIterate(scrapeFiles)
+function fileIterate(scrapeFiles) {
+	for (let i = 0; i <= scrapeFiles.length; i++) {
+		if (i === scrapeFiles.length) {
+			makeGeoJSON(vtFlights);
+		} else {
+			// Var savefile = 'dump/2018-05-16/vt/' + path.basename(filename, '.json') + '-vt.json'
+			const savefile = 'dump/2018-05-16/' + path.basename(scrapeFiles[i]);
+			if (fs.existsSync(savefile)) {
+				console.log(path.basename(savefile, '.json') + ' already exists');
+			} else {
+				console.log('Reading ' + scrapeFiles[i]);
+				const file = fs.readFileSync(scrapeFiles[i], 'utf-8');
+				vermontFilter(file, vtbbox, vtFlights);
+			}
+		}
+	}
 }
 
-function fileIterate (scrapeFiles) {
-  for (var i = 0; i <= scrapeFiles.length; i++) {
-    if (i == scrapeFiles.length) {
-      makePoints(allCoords)
-    } else {
-      var AircraftList = JSON.parse(fs.readFileSync(scrapeFiles[i], 'utf-8'))
-      var acList = AircraftList.acList
-      getCoords(acList)
-    }
-  }
+function vermontFilter(file, vtbbox, vtFlights) {
+	try {
+		const AircraftList = JSON.parse(file);
+		AircraftList.map(flight => {
+			if (flight.Lat && flight.Long) {
+				if (flight.Lat >= vtbbox.lat.min && flight.Lat <= vtbbox.lat.max &&
+					flight.Long >= vtbbox.long.min && flight.Long <= vtbbox.long.max) {
+					createCoords(flight, vtFlights);
+				}
+				return false;
+			}
+			return false;
+		});
+		console.log('Found ' + Object.keys(vtFlights).length + ' flights in VT.');
+	} catch (e) {
+		console.log(e);
+	}
 }
 
-function getCoords(acList){
-  for (var i = 0; i < acList.length; i++) {
-    var flight = acList[i]
-    if (flight.Lat && flight.Long) {
-      var pair = [flight.Long, flight.Lat]
-      allCoords.push(pair)
-    }
-  }
+function createCoords(vtFlight, vtFlights) {
+	if (vtFlight.Cos) {
+		if (!vtFlights[vtFlight.Id]) {
+			vtFlights[vtFlight.Id] = {
+				type: 'Feature',
+				properties: {
+					id: vtFlight.Id
+				},
+				geometry: {
+					type: 'LineString',
+					coordinates: []
+				}
+			};
+		}
+		for (let j = 0; j <= vtFlight.Cos.length; j += 4) {
+			const lon = vtFlight.Cos[j + 1];
+			const lat = vtFlight.Cos[j];
+			if (lon && lat) {
+				vtFlights[vtFlight.Id].geometry.coordinates.push([lon, lat]);
+			} else {
+				console.log('Missing coords');
+			}
+		}
+	}
 }
 
-function makePoints(allCoords) {
-  var pointFeatures = allCoords.map((item) => {
-    var point = turf.point(item)
-    return turf.point(item)
-  })
-  for (var i = 0; i <= pointFeatures.length; i++) {
-    if (i == pointFeatures.length) {
-      console.log(geojson);
-      fs.writeFile('vt-flights.geojson', JSON.stringify(geojson, null, 2), 'utf-8', (err) => {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log('file written');
-        }
-      } )
-    } else {
-      geojson.features.push(pointFeatures[i])
-    }
-  }
-}
-
-function filterKBTV(t) {
-  var u = t.AircraftList
-  let kbtv_flights = u.filter(function(item) {
-    if (item.From && item.To) {
-      if (item.From.substring(0, 4) == 'KBTV' || item.To.substring(0, 4) == 'KBTV') {
-        return item
-      }
-    }
-  })
-  console.log(kbtv_flights.length);
+function makeGeoJSON(vtFlights) {
+	Object.keys(vtFlights).forEach(item => {
+		geojson.features.push(vtFlights[item]);
+	});
+	fs.writeFileSync('VT-flights.geojson', JSON.stringify(geojson, null, 2));
 }
