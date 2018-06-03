96dbf8dc2092ad5789e18d00ee689bcb78e6476f
Stephen Smith <stephen@mapsmith.net>
2018-06-02 23:42:36 -0400

    Remove lineStringValidate
    Move coordinate checking to createCoords function
    redo makeGeoJSON function to push all objets to geoJSON features array

--- adsbexchange/aclist-merge.js
+++ adsbexchange/aclist-merge.js
@@ -43,7 +43,7 @@ function fileIterate(scrapeFiles) {
 	const len = scrapeFiles.length;
 	for (let i = 0; i <= len; i++) {
 		if (i === len) {
-			lineStringValidate(vtFlights);
+			makeGeoJSON(vtFlights);
 		} else {
 			const file = fs.readFileSync(scrapeFiles[i], 'utf-8');
 			try {
@@ -63,7 +63,7 @@ function vermontFilter(acList, count) {
 		if (flight.Lat && flight.Long) {
 			if (flight.Lat >= bbox.lat.min && flight.Lat <= bbox.lat.max &&
 					flight.Long >= bbox.long.min && flight.Long <= bbox.long.max) {
-				createCoords(flight);
+				setProperties(flight);
 			}
 			return false;
 		}
@@ -72,12 +72,14 @@ function vermontFilter(acList, count) {
 	console.log('Found ' + Object.keys(vtFlights).length + ' flights in bbox. (' + count + ')');
 }
 
-function createCoords(vtFlight) {
+function setProperties(vtFlight) {
 	if (vtFlight.Cos) {
-		const uid = vtFlight.Id + '-' + vtFlight.PosTime;
 		const id = vtFlight.Id;
+		const timestamp = vtFlight.PosTime;
 		const alt = vtFlight.Alt;
+
 		let color = '#263D4C';
+
 		if (vtFlight.Alt < 10000) {
 			color = '#23B5F5';
 		} else if (vtFlight.Alt > 10000 && vtFlight.Alt < 20000) {
@@ -85,61 +87,51 @@ function createCoords(vtFlight) {
 		} else if (vtFlight.Alt > 20000 && vtFlight.Alt < 30000) {
 			color = '#35627D';
 		}
+
 		if (!vtFlights[id]) {
 			vtFlights[id] = {};
 		}
-		if (!vtFlights[id][uid]) {
-			vtFlights[id][uid] = {
-				type: 'Feature',
-				properties: {
-					id,
-					uid,
-					alt,
-					color
-				},
-				geometry: {
-					type: 'LineString',
-					coordinates: []
-				}
-			};
-		}
-		for (let j = 0; j < vtFlight.Cos.length; j += 4) {
-			const lon = vtFlight.Cos[j + 1];
-			const lat = vtFlight.Cos[j];
-			if (lon && lat && vtFlights[id][uid]) {
-				if (vtFlights[id][uid].geometry.coordinates.indexOf([lon, lat]) === -1) {
-					vtFlights[id][uid].geometry.coordinates.push([lon, lat]);
-				} else {
-					console.log('Matches');
-				}
-			} else {
-				console.log('something else');
+
+		vtFlights[id][timestamp] = {
+			type: 'Feature',
+			properties: {
+				timestamp,
+				id,
+				alt,
+				color
+			},
+			geometry: {
+				type: 'LineString',
+				coordinates: []
 			}
+		};
+		createCoords(vtFlight, id, timestamp);
+	}
+
+	function createCoords(vtFlight, id, timestamp) {
+		if (vtFlight.Cos.length >= 8) {
+			for (let j = 0; j <= vtFlight.Cos.length; j += 4) {
+				const lon = vtFlight.Cos[j + 1];
+				const lat = vtFlight.Cos[j];
+				if (lon && lat) {
+					vtFlights[id][timestamp].geometry.coordinates.push([lon, lat]);
+				}
+			}
+		} else {
+			console.log(`Not enough coordinates for ${id} + ${timestamp}.`);
 		}
 	}
 }
 
-function lineStringValidate(vtFlights) {
-	const foundIds = Object.values(vtFlights);
-	const validLines = [];
-	foundIds.forEach(item => {
-		const j = Object.values(item);
-		const vl = j.filter(item => {
-			if (item.geometry.coordinates.length > 1) {
-				return item;
-			}
-		});
-		for (let i = 0; i < vl.length; i++) {
-			validLines.push(vl[i]);
+function makeGeoJSON(vtFlights) {
+	for (let i = 0; i < Object.keys(vtFlights).length; i++) {
+		const key = Object.keys(vtFlights)[i];
+		const ts = Object.keys(vtFlights[key]);
+		for (let k = 0; k < ts.length; k++) {
+			geojson.features.push(vtFlights[key][ts[k]]);
 		}
-	});
-	makeGeoJSON(validLines);
-}
+	}
 
-function makeGeoJSON(validLines) {
-	validLines.forEach(item => {
-		geojson.features.push(item);
-	});
 	const geojsonReadStream = new Readable();
 	const geojsonWriteStream = fs.createWriteStream('./stream-color.geojson');
 	geojsonReadStream.push(JSON.stringify(geojson, null, 2));
