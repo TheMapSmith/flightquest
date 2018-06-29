21e2fd1dd6658a20fc988584fedae6464735fbcd
Stephen Smith <stephen@mapsmith.net>
2018-06-29 17:30:00 -0400

    add Altitude property to each line segment

--- adsbexchange/aclist-merge.js
+++ adsbexchange/aclist-merge.js
@@ -4,7 +4,7 @@ const {Readable} = require('stream');
 
 const dumpFolder = 'dump/2018-06-01/';
 const dev = 'OD';
-const airports = ['KDFW'];
+const airports = ['KORD'];
 
 const vtFlights = {};
 
@@ -45,7 +45,7 @@ function fileIterate(scrapeFiles) {
 	const len = scrapeFiles.length;
 	for (let i = 0; i <= len; i++) {
 		if (i === len) {
-			makeGeoJSON(vtFlights);
+			makeLineString(vtFlights);
 		} else {
 			const file = fs.readFileSync(scrapeFiles[i], 'utf-8');
 			try {
@@ -148,38 +148,46 @@ function createObject(vtFlight) {
 	}
 }
 
-function makeGeoJSON(vtFlights) {
-	for (let i = 0; i < Object.keys(vtFlights).length; i++) {
-		const key = Object.keys(vtFlights)[i];
-		const flight = vtFlights[key];
-		const [...Cos] = flight.Cos;
-		let prevAlt = 0;
-		let coordinates = [];
-		for (let m = 0; m <= Cos.length; m++) {
-			if (m === Cos.length) {
-				flight.geometry.coordinates = coordinates;
-				delete flight.Cos;
-				geojson.features.push(flight);
-			} else {
-				const coordPair = [Cos[m].lon, Cos[m].lat];
-				const alt = Cos[m].alt;
-				if (m === 0) {
-					coordinates.push(coordPair);
-				} else if (m > 0 && Math.abs(alt - prevAlt) > 2000 && coordinates.length > 1) {
+function makeLineString(vtFlights) {
+	for (let i = 0; i <= Object.keys(vtFlights).length; i++) {
+		if (i === Object.keys(vtFlights).length) {
+			makeGeoJSON();
+		} else {
+			const key = Object.keys(vtFlights)[i];
+			const flight = vtFlights[key];
+			const [...Cos] = flight.Cos;
+			let prevAlt = 0;
+			let coordinates = [];
+			for (let m = 0; m <= Cos.length; m++) {
+				if (m === Cos.length) {
 					flight.geometry.coordinates = coordinates;
 					delete flight.Cos;
 					geojson.features.push(flight);
-					flight.geometry.coordinates = [];
-					coordinates = [];
-					coordinates.push(coordPair);
 				} else {
-					coordinates.push(coordPair);
+					const coordPair = [Cos[m].lon, Cos[m].lat];
+					const alt = Cos[m].alt;
+					if (m === 0) {
+						flight.properties.alt = alt;
+						coordinates.push(coordPair);
+					} else if (m > 0 && Math.abs(alt - prevAlt) > 2000 && coordinates.length > 1) {
+						flight.geometry.coordinates = coordinates;
+						flight.properties.alt = alt;
+						delete flight.Cos;
+						geojson.features.push(flight);
+						flight.geometry.coordinates = [];
+						coordinates = [];
+						coordinates.push(coordPair);
+					} else {
+						coordinates.push(coordPair);
+					}
+					prevAlt = alt;
 				}
-				prevAlt = alt;
 			}
 		}
 	}
+}
 
+function makeGeoJSON() {
 	const geojsonReadStream = new Readable();
 	let filename = '';
 	for (let i = 0; i < airports.length; i++) {
